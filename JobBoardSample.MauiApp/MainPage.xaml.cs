6787edb50f0da6dc50c19062cb13625ff698902f
Michele Boninelli <m.boni2000@gmail.com>
2025-08-28 10:20:17 +0200

    Inizialized Project
    - created projects
    - inizialized class

---
+++ JobBoardSample.MauiApp/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace JobBoardSample.MauiApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ JobBoardSample.Shared/Position.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobBoardSample.Shared
+{
+    public class Position
+    {
+        public int Id {  get; set; }
+        public string Title { get; set; } = "";
+        public string Department { get; set; } = "";
+        public string Location { get; set; } = "";
+        public decimal SalaryRangeMin { get; set; }
+        public decimal SalaryRangeMax { get; set; }
+        public DateTime PublishedAtUtc { get; set; }
+    }
+}
